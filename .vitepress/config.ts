652c297071e3932519f80e84a021c4305128117e
EGOIST <hi@egoist.dev>
2023-08-01 17:20:38 +0800

    init

---
+++ .vitepress/config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Kenja Docs",
+  description: "Documentation for Kenja",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: 'https://kenja.app' },
+      { text: 'Docs', link: '/' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [
+          {text:'Bot', link: '/bot'},
+          {text: 'Source', link: '/source'}
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ index.md
@@ -0,0 +1,7 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+
+---
+
+
+Welcome to the Kenja Documentation!
