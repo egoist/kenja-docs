9aaff374fbb78a0fb08233f603e36989efc25a33
EGOIST <hi@egoist.dev>
2023-08-17 14:33:17 +0800

    Update config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/egoit/kenja-docs' }
+      { icon: 'github', link: 'https://github.com/egoist/kenja-docs' }
     ]
   }
 })
